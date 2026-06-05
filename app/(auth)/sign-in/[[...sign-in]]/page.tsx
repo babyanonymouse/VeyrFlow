a25d91fa1551c6668f1503bd32dac0a555672055
Anonymouse Baby <babyanonymouse2@gmail.com>
2026-06-05 07:20:03 +0300

    feat: Replace static imports with dynamic imports for SignIn and SignUp components

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,4 +1,8 @@
-import { SignIn } from "@clerk/nextjs";
+"use client";
+
+import dynamic from "next/dynamic";
+
+const SignIn = dynamic(() => import("@clerk/nextjs").then((mod) => mod.SignIn), { ssr: false });
 
 export default function SignInPage() {
   return (
